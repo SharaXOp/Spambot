3e2f8ce37731708674eed271dc2a4e20df2076eb
Shara X Op <160161353+SharaXOp@users.noreply.github.com>
2024-03-09 11:55:41 +0530

    Create start.py

---
+++ AltronX/modules/start.py
@@ -0,0 +1,44 @@
+from telethon import events, Button
+from config import MK1, MK2, MK3, MK4, MK5, MK6, MK7, MK8, MK9, MK10
+from AltronX.modules.help import *
+import telethon
+
+PythonButton = [
+        [
+        Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
+        ],
+        [
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/SharaXAssociation"),
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/DangerX_zone")
+        ],
+        [
+        Button.url("• ʀᴇᴘᴏ •", "https://github.com/SharaXOp/Spambot")
+        ]
+        ]
+
+
+@MK1.on(events.NewMessage(pattern="/start"))
+@MK2.on(events.NewMessage(pattern="/start"))
+@MK3.on(events.NewMessage(pattern="/start"))
+@MK4.on(events.NewMessage(pattern="/start"))
+@MK5.on(events.NewMessage(pattern="/start"))
+@MK6.on(events.NewMessage(pattern="/start"))
+@MK7.on(events.NewMessage(pattern="/start"))
+@MK7.on(events.NewMessage(pattern="/start"))
+@MK8.on(events.NewMessage(pattern="/start"))
+@MK9.on(events.NewMessage(pattern="/start"))
+@MK10.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+        AltBot = await event.client.get_me()
+        BotName = AltBot.first_name
+        BotId = AltBot.id
+        TEXT = f"**ʜᴇʏ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{BotName}](tg://user?id={BotId})**\n━━━━━━━━━━━━━━━━━━━\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ : [ʟᴇɢᴇɴᴅ](https://t.me/SHARA_XD)**\n\n"
+        TEXT += f"» **ʙᴏᴛ ꜱᴘᴀᴍ ᴠᴇʀsɪᴏɴ :** `M3.2`\n"
+        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{telethon.__version__}`\n━━━━━━━━━━━━━━━━━"
+        await event.client.send_file(
+                event.chat_id,
+                "https://telegra.ph/file/b454ac4d320622facf8ca.jpg",
+                caption=TEXT, 
+                buttons=PythonButton)
